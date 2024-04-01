9a7941a53f84fd838e78b6de194e5d82d0fb1ac6
xKakaxD <ti.demoro@gmail.com>
2024-03-31 23:39:12 -0300

    test: realizando testes para tentar fazer a tela nao mover com o menu aberto, foi comentado

--- app/_js/menu.js
+++ app/_js/menu.js
@@ -1,15 +1,15 @@
 function toggleMenu() {
     var sidebar = document.getElementById("sidebar");
     var sidebarLeft = window.getComputedStyle(sidebar).getPropertyValue("left");
-    var body = document.body;//tentando fazer a tela nao mover quando aberta a barra
+    //var body = document.body; tentando fazer a tela nao mover quando aberta a barra
 
     if (sidebarLeft === "-300px") {
         sidebar.style.left = "0"; // Mostra o menu lateral
-        body.classList.add("sidebar-open");
-        body.classList.remove("sidebar-closed");
+       // body.classList.add("sidebar-open");
+       // body.classList.remove("sidebar-closed");
     } else {
         sidebar.style.left = "-300px"; // Esconde o menu lateral
-        body.classList.add("sidebar-closed");
-        body.classList.remove("sidebar-open");
+       // body.classList.add("sidebar-closed");
+       // body.classList.remove("sidebar-open");
     }
 }
