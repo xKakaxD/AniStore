98c0000af25e431fb51ff8d8d8a006b3e3272325
xKakaxD <ti.demoro@gmail.com>
2024-06-01 11:51:27 -0300

    chore: remoção de linhas desnecesárias

--- app/_js/menu.js
+++ app/_js/menu.js
@@ -1,15 +1,10 @@
 function toggleMenu() {
     var sidebar = document.getElementById("sidebar");
     var sidebarLeft = window.getComputedStyle(sidebar).getPropertyValue("left");
-    //var body = document.body; tentando fazer a tela nao mover quando aberta a barra
 
     if (sidebarLeft === "-301px") {
         sidebar.style.left = "0"; // Mostra o menu lateral
-       // body.classList.add("sidebar-open");
-       // body.classList.remove("sidebar-closed");
     } else {
         sidebar.style.left = "-301px"; // Esconde o menu lateral
-       // body.classList.add("sidebar-closed");
-       // body.classList.remove("sidebar-open");
     }
 }
