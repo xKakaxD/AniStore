911d9a5ca1cfe990b6b8a0fdca298c8e713991ef
xKakaxD <ti.demoro@gmail.com>
2024-09-12 15:42:20 -0300

    Novos dados pro BD

--- app/_migrations/migration-06062024-001.sql
+++ app/_migrations/migration-06062024-001.sql
@@ -20,6 +20,6 @@ INSERT INTO tb_produtos (
  ('Almofada Luffy Gear 5', 'Macia e flexivel como borracha para maior conforto', 'https://i.imgur.com/4hEhFgf.jpeg', 28.90, 1, 4, FALSE),
  ('Camisa Sakura', 'Uma blusa que destaca sua beleza e força', 'https://i.imgur.com/3u9gn5f.jpeg', 90.00, 2, 12, FALSE),
  ('Camisa Sasuke', 'Seja forte e estiloso com essa novidade', 'https://i.imgur.com/lGrfCOC.jpeg', 90.00, 2, 26, FALSE),
- ('Firure action Crollo', 'Lider da trupe Fantasma', 'https://i.imgur.com/gOZNfWX.jpeg', 50.00, 1, 5, FALSE),
+ ('Figure action Crollo', 'Lider da trupe Fantasma', 'https://i.imgur.com/gOZNfWX.jpeg', 50.00, 1, 5, FALSE),
  ('Figure action Killua ', 'Killua Zoldyck', 'https://i.imgur.com/VWcf8Sr.jpeg', 50.00, 1, 8, FALSE)
 ;
---
+++ app/_migrations/migration-10062024-001.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_produtos (
+    nome, descricao, image_url, valor, id_categoria, estoque, destaque
+) VALUES
+ ('PRODUTO AULA', 'Chapéu do futuro Rei dos Piratas', 'https://i.imgur.com/epLbIvi.png', 70.00, 2, 11, TRUE);
